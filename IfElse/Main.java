c4fb75fba58fdd92599fe05115d94846f4b1f606
Will <weosborne90@gmail.com>
2022-11-29 15:27:07 -0500

    Just practice with Git

---
+++ IfElse/Main.java
@@ -0,0 +1,29 @@
+package IfElse;
+
+import java.util.*;
+
+public class Main {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 == 1){
+            System.out.println("Weird");
+        } else if (N % 2 != 1){
+            if (N <= 5) {
+                System.out.println("Not Weird");
+            } else if (N <= 20){
+                System.out.println("Weird");
+            } else {
+                System.out.println("Not Weird");
+            }
+        }
+
+        scanner.close();
+    }
+}
